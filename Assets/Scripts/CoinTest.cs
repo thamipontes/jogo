ce4c49974218f0c2fa5fb652279672b463443454
aluzianobriceno <aluzianobriceno@gmail.com>
2020-12-29 15:51:15 -0300

    Prefab e cenario

---
+++ Assets/Scripts/CoinTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTest : MonoBehaviour
+{
+
+    //public int coins = 0;
+    
+
+    //public Text CoinText;
+
+    
+    void Start()
+    {
+    //    CoinText.text = "Moedas: " + coins;     
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {        
+         
+            if(other.gameObject.CompareTag("Moeda")){
+            //    coins++;
+            //    CoinText.text = "Moedas: " + coins; 
+                Destroy(other.gameObject);
+            }       
+
+        } 
+}
