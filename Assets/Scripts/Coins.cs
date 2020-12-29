4ffea5d6db3fcb1c6c2e0dd7a6d5361453385e4b
Thamires <thamiresdepontesdasilva@gmail.com>
2020-12-28 22:06:56 -0300

    Script Moedas

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+
+    public int coins = 0;
+    
+
+    public Text CoinText;
+
+    
+    void Start()
+    {
+        CoinText.text = "Moedas: " + coins;     
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {        
+         
+            if(other.gameObject.CompareTag("Moeda")){
+                coins++;
+                CoinText.text = "Moedas: " + coins; 
+                Destroy(other.gameObject);
+            }       
+
+        }      
+}
+
