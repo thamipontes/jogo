43ba1bfff53133fba4a6918bd0d53ebda9c7cc6e
Thamires <thamiresdepontesdasilva@gmail.com>
2021-01-17 21:13:50 -0300

    Script Ponte

---
+++ Assets/Scripts/Ponte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ponte : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D _rigidbody;
+    public float timeForFall = 0.5f;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Fall();
+            // Debug.Log("Entrei no tempo");
+            
+
+        }
+    }
+    
+    public void Fall()
+    {
+        StartCoroutine(FallTime());
+    }
+
+    IEnumerator FallTime()
+    {
+        Debug.Log("Entrei");
+        yield return new WaitForSeconds(timeForFall);
+        _rigidbody.gravityScale = 1f;
+        Debug.Log("Sai");
+        
+    }
+}
