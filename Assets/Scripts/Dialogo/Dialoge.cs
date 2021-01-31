fcfa660f432f1483d9bf5e6cecb5f3cb2edbb248
Ana Sofia <anasofialaranja@gmail.com>
2021-01-30 21:18:02 -0300

    caixa de dialogo Co-authored-by: pedrinha <thamiresdepontesdasilva@gmail.com> Co-authored-by: Ana Julia <aluzianobriceno@gmail.com>

---
+++ Assets/Scripts/Dialogo/Dialoge.cs
@@ -0,0 +1,13 @@
+
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Dialoge
+{
+  public string name;
+  [TextArea(3,10)]  
+  public string[] sentence;
+}
