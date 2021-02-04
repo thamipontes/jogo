fa08c682814d8e318196c098bd0c8fb708cf64c2
Ana Sofia <anasofialaranja@gmail.com>
2021-02-03 22:05:10 -0300

    primeiro gatilho do dialogo Co-authored-by: Thamires <thamiresdepontesdasilva@gmail.com>

---
+++ Assets/Scripts/ConversationAda.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConversationAda : MonoBehaviour
+{
+    [SerializeField] Tester _tester;
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      if(other.gameObject.CompareTag("Player"))
+      {
+        _tester.StartConvo();
+      }
+    }
+}
