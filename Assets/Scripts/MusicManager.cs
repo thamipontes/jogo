1d29a0033b9e1a6b30ea4eb64a04fc7885ab5936
Thamires <thamiresdepontesdasilva@gmail.com>
2021-05-07 21:48:29 -0300

    Aplica Musica Moedas

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static AudioClip _audioCoin, _audioPulo;
+    public static AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioCoin = Resources.Load<AudioClip>("MOEDAS_01");
+        _audioPulo = Resources.Load<AudioClip>("PULO_01");
+        
+    }
+
+    public static void playSound (string som)
+    {
+        switch (som)
+        {
+            case "coin1":
+                _audioSource.PlayOneShot(_audioCoin);
+                break;
+            case "pulo1":
+                _audioSource.PlayOneShot(_audioPulo);
+                break;
+        }
+    }
+
+
+    // public static void comprasShein(string roupa)
+    // {
+    //     switch (roupa)
+    //     {
+    //         case "vestido": print("Tais");
+    //             break;
+    //         case "jeans": print("Ana Sofia");
+    //             break;
+    //         case "short": print("Ana julia");
+    //             break;
+    //         default: print("Hanani");
+    //             break;
+    //     }
+    // }
+
+    // bool somosBonitas()
+    // {
+    //     return true;
+    // }
+
+    // void elogio()
+    // {
+    //     printf("SOMOS GOSTOSAS");
+    // }
+
+    // int idadeAnaSofia()
+    // {
+    //     return 85;
+    // }
+}
