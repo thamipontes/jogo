392fdda97c33c33c3959523cd1625dd381718957
Ana Sofia <anasofialaranja@gmail.com>
2021-06-15 19:57:25 -0300

    volta um commit

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DontDestroy : MonoBehaviour
 {
-    private GameObject[] music;
-   
-    void Start(){
-        music = GameObject.FindGameObjectsWithTag ("Music");
-        Destroy (music[1]);
-    }
-    
-    // Update is called once per frame
-    void Awake () {
-        DontDestroyOnLoad (transform.gameObject);
+    void Awake() {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1) {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
     }
 }
