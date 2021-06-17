c998d7c33d070bee22e6679622d4d4782153ff05
Ana Sofia <anasofialaranja@gmail.com>
2021-06-17 19:46:54 -0300

    Conserta botão de música de fundo Co-authored-by: Tais-A <dtais@outlook.com.br> Co-authored-by: Ana Julia <aluzianobriceno@gmail.com>

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class DontDestroy : MonoBehaviour
 {
+    public static DontDestroy instance;
+
     void Awake() {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
-
-        if (objs.Length > 1) {
-            Destroy(this.gameObject);
-        }
-
-        DontDestroyOnLoad(this.gameObject);
+      if(instance == null) {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+      }
+      else {
+        print("ahora estamos destruindo o canvas");
+        Destroy(gameObject);
+      }
     }
 }
