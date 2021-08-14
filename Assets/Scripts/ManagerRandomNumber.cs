7f58799144d838e538b920932252b23137578617
anasofia0 <anasofialaranja@gmail.com>
2021-08-13 21:10:37 -0300

    [WIP] anasofia0/ATA-2 modificação UI Co-authored-by: Ana Julia <aluzianobriceno@gmail.com> Co-authored-by: Thamires <thamiresdepontesdasilva@gmail.com>

---
+++ Assets/Scripts/ManagerRandomNumber.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerRandomNumber : MonoBehaviour
+{
+    public char[] operadores = {'+', '-', '*'};
+    public int resultado;
+
+    private SelecionaOperador(int quantOperador){
+      
+    }
+}
---
+++ Assets/Scripts/ManagerToggle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ManagerToggle : MonoBehaviour
+{
+    public TextMeshProUGUI numero;
+    public Toggle[] toggle = new Toggle[5];
+
+    private int canPressToggle = 1;
+
+    private void Update()
+    {
+        for(int j = 0; j < toggle.Length; j++){
+            if(toggle[j].isOn == true && canPressToggle == 1) {
+                numero.text = (j+1) + "";
+                canPressToggle = 0;
+                for(int i = 0; i < toggle.Length; i++)
+                {
+                    toggle[i].interactable = false;
+                }
+            }
+        }
+    }
+}
