d4177556dc0f5d4f5d611e9b26152ba294c9e28d
anasofia0 <anasofialaranja@gmail.com>
2021-08-20 20:52:06 -0300

    [WIP] anasofia0/ATA-2 implementa randomicidade dos operadores do puzzle Co-authored-by: Ana Julia <aluzianobriceno@gmail.com> Co-authored-by: Thamires <thamiresdepontesdasilva@gmail.com>

--- Assets/Scripts/ManagerRandomNumber.cs
+++ Assets/Scripts/ManagerRandomNumber.cs
@@ -1,13 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManagerRandomNumber : MonoBehaviour
 {
-    public char[] operadores = {'+', '-', '*'};
-    public int resultado;
+    [SerializeField] Text[] sinaisCanvas = new Text[5];
+
+    private char[] operadores = {'+', '-'};
+    private int resultado, estado = 1;
+    private string sinal;
+
+    void Start(){
+        switch (estado){
+            case 1:
+                sinaisCanvas[0].text = operadores[0].ToString();
+                for(int i = 1; i < 4; i++){
+                    sinaisCanvas[i].text = "";
+                }
+                sinaisCanvas[4].text = SelecionaValorResultado(0,2).ToString("0");
+                break;
+        }        
+    }
+
+    void Update(){
+
+    }
 
     private void SelecionaOperador(int quantOperador){
-      
+        if(estado < 4){
+            sinal = operadores[Random.Range(0,1)] + "";
+        }
+    }
+
+    public int SelecionaValorResultado(int numeroSinaiNeg, int numeroEspacos){
+        
+        int valorMaximo = 5*(numeroEspacos - numeroSinaiNeg) - numeroSinaiNeg;
+        int valorMinimo;
+        if(numeroSinaiNeg == 0){
+            valorMinimo = numeroEspacos;
+        } else {
+            valorMinimo = 0;
+        }
+
+        return Random.Range(valorMinimo, valorMaximo);
     }
 }
