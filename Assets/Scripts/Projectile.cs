aa9451f1216f75a4f32d3c5afa433c7135f03392
Thamires <thamiresdepontesdasilva@gmail.com>
2021-11-30 15:33:11 -0300

    :construction: Boss esta soltando laser

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+    
+    private void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+    
+
+    public float timeThrowPena = 6f;
+
+    public float time = 2f;
+    
+    void Update()
+    {
+        if (_particleSystem.isStopped)
+        {
+            StartCoroutine(timeToPlay());
+        }
+    }
+    
+    IEnumerator timeToPlay()
+    {
+        yield return new WaitForSecondsRealtime(0.75f);
+        _particleSystem.Play();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            var healthPlayer = other.gameObject.GetComponent<PlayerHealth>();
+            healthPlayer.Damage();
+        }
+    }
+}
