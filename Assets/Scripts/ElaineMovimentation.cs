38d13711dea660dc1de31e83d9a0268021bb2eec
BiancaPatrocinio7 <biancapatrocinio774@gmail.com>
2022-04-18 20:48:15 -0300

    agora vai

---
+++ Assets/Scripts/ElaineMovimentation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace PartII {
+	public class ElaineMovimentation : MonoBehaviour
+	{
+		public float speed = 1.0f;
+		public float jump = 1.0f;
+		// Public attributes
+		void Update (){
+			float horizontal = Input.GetAxis("Horizontal");
+			transform.Translate(new Vector3(horizontal*speed*Time.deltaTime, 0, 0));
+			//float jump = Imput.GetAxis("Jump");
+		//	transform.Translate(new Vector2(rigidBody2D.velocity.x, 0);
+			//animator.SetBool("taPulando", true);
+			
+
+
+		}
+
+	
+	}
+}
