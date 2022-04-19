4331201a9498ff0034453f722f14962e9f4aa26d
Hanani_UnB <hanani.emanuelle@gmail.com>
2022-04-18 22:07:45 -0300

    colocamos o teste de pulo

--- Assets/Scripts/ElaineMovimentation.cs
+++ Assets/Scripts/ElaineMovimentation.cs
@@ -6,11 +6,15 @@ namespace PartII {
 	{
 		public float speed = 1.0f;
 		public float jump = 1.0f;
+		public Rigidbody2D personagem;
 		// Public attributes
 		void Update (){
 			float horizontal = Input.GetAxis("Horizontal");
 			transform.Translate(new Vector3(horizontal*speed*Time.deltaTime, 0, 0));
-			//float jump = Imput.GetAxis("Jump");
+			float getJump = Input.GetAxis("Jump"); //pega um momento espcifico
+				if (getJump > 0) {
+					personagem.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+				}
 		//	transform.Translate(new Vector2(rigidBody2D.velocity.x, 0);
 			//animator.SetBool("taPulando", true);
 			
